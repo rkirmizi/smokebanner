25e6d954402804499d15c3ec452ee7b159d9d009
Recep KIRMIZI <rkirmizi@gmail.com>
2013-03-12 03:54:49 +0200

    bunu duzeltelim

--- nosmoking/smokebanner/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from smokebanner.models import Banner
-
-class BannerAdmin(admin.ModelAdmin):
-    list_display = ['hours', 'cost', 'quantity', 'email']
-
-admin.site.register(Banner, BannerAdmin)
